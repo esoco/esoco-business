48b1a5e4dffbde159ed3af843b010c4513843c88
Elmar Sonnenschein <eso@esoco.de>
2016-11-25 11:11:02 +0100

    Java 8 support

--- src/main/java/de/esoco/entity/EntityPredicates.java
+++ src/main/java/de/esoco/entity/EntityPredicates.java
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -79,7 +79,7 @@ public class EntityPredicates
 	 *
 	 * @see Predicates#ifRelation(RelationType, Predicate)
 	 */
-	public static <E extends Entity, V> Predicate<E> ifAttribute(
+	public static <E extends Entity, V> ElementPredicate<E, V> ifAttribute(
 		RelationType<V>		 rType,
 		Predicate<? super V> rPredicate)
 	{
@@ -117,8 +117,8 @@ public class EntityPredicates
 	 *
 	 * @return A new predicate for the given extra attribute
 	 */
-	public static <E extends Entity, V> Predicate<E> ifHasExtraAttribute(
-		Class<? extends Entity>			  rEntityType,
+	public static <E extends Entity, V> ElementPredicate<E, V> ifHasExtraAttribute(
+		Class<E>						  rEntityType,
 		Predicate<? super ExtraAttribute> pCriteria)
 	{
 		EntityDefinition<? extends Entity> rDef =
@@ -139,9 +139,11 @@ public class EntityPredicates
 						   ifAttribute(ExtraAttribute.ENTITY,
 									   like(sIdPrefix + "%")));
 
-		return ifAttribute(rDef.getIdAttribute(),
-						   refersTo(ExtraAttribute.class,
-									fExtraAttributeEntityId,
-									pCriteria));
+		RelationType<Integer> rIdAttr = rDef.getIdAttribute();
+
+		Predicate<ExtraAttribute> pHasExtraAttr =
+			refersTo(ExtraAttribute.class, fExtraAttributeEntityId, pCriteria);
+
+		return ifAttribute(rIdAttr, pHasExtraAttr);
 	}
 }
--- src/test/java/de/esoco/entity/EntityStorageTest.java
+++ src/test/java/de/esoco/entity/EntityStorageTest.java
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +16,7 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.entity;
 
-import de.esoco.lib.expression.Predicate;
+import de.esoco.lib.expression.predicate.ElementPredicate;
 import de.esoco.lib.manage.TransactionException;
 
 import de.esoco.storage.StorageException;
@@ -396,7 +396,8 @@ public class EntityStorageTest extends AbstractEntityStorageTest
 	@Test
 	public void testSortedQuery() throws StorageException
 	{
-		Predicate<Entity> aSortPredicate = ifAttribute(FORENAME, alwaysTrue());
+		ElementPredicate<Entity, String> aSortPredicate =
+			ifAttribute(FORENAME, alwaysTrue());
 
 		aSortPredicate.set(MetaTypes.SORT_ASCENDING, true);
 
