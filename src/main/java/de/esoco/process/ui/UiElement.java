a43547444b964e66068e983f0e6285ab40ab55f1
Elmar Sonnenschein <eso@esoco.de>
2018-05-01 09:34:18 +0200

    OPT: modification tracking

--- src/main/java/de/esoco/process/ui/UiElement.java
+++ src/main/java/de/esoco/process/ui/UiElement.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-business' project.
-// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -236,6 +236,6 @@ public class UiElement<E extends UiElement<E>>
 	final void setProperties(MutableProperties rNewProperties)
 	{
 		aProperties = rNewProperties;
-		bModified   = true;
+		setModified(true);
 	}
 }
