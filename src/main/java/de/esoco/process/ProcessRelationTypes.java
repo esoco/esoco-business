28e79474d82f923ffecc4eb8454fb8819f2f02bb
Elmar Sonnenschein <eso@esoco.de>
2018-07-12 11:44:52 +0200

    RF: relation type default value function

--- src/main/java/de/esoco/process/ProcessRelationTypes.java
+++ src/main/java/de/esoco/process/ProcessRelationTypes.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'esoco-business' project.
-// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -586,7 +586,8 @@ public class ProcessRelationTypes
 		RelationType<T> aDerivedType =
 			RelationTypes.<T>newRelationType(aName.toString(),
 											 rRelationType.getValueType(),
-											 rRelationType.getDefaultValue(),
+											 rRelationType
+											 .getDefaultValueFunction(),
 											 rRelationType
 											 .getInitialValueFunction());
 
